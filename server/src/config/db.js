c571917278b1ec542e55a8136f8af07529ef2987
venunisandhan <venunisandhan@gmail.com>
2026-07-09 17:07:40 +0530

    chore : added global logging and added database connection config

---
+++ server/src/config/db.js
@@ -0,0 +1,18 @@
+
+const moongoose = require('mongoose');
+
+const env = require('./env');
+
+const logger = require('../utils/logger');
+
+async function connectDB() {
+    try{
+        await mongoose.connect(env.MONGO_URI);
+        logger.info("MongoDB connected");
+    }catch(err){
+        logger.error('MongoDB connection failed',{error:err.message});
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
