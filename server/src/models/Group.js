6881906d17946f416dade35b112fb24295d124e9
venunisandhan <venunisandhan@gmail.com>
2026-07-10 23:59:01 +0530

    chore : added group model

---
+++ server/src/models/Group.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Group name is required'],
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+groupSchema.index({ createdBy: 1, name: 1 });
+
+module.exports = mongoose.model('Group', groupSchema);
