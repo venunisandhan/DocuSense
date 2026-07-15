29c7135674e487d5794c047be0fe544c597cf282
venunisandhan <venunisandhan@gmail.com>
2026-07-15 14:39:58 +0530

    feat : add doucmentChunk model

---
+++ server/src/models/DocumentChunk.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+
+const documentChunkSchema = new mongoose.Schema(
+  {
+    document: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Document',
+      required: true,
+    },
+    chunkIndex: {
+      type: Number,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    embedding: {
+      type: [Number],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+documentChunkSchema.index({ document: 1, chunkIndex: 1 });
+
+module.exports = mongoose.model('DocumentChunk', documentChunkSchema);
